ffdd39b2a18716d12d2256e04040c6a46c21d3a4
zlyck <65764533+zlyck@users.noreply.github.com>
2020-06-12 18:31:57 +0700

    Add files via upload

---
+++ precache-manifest.7bbe8c8e92654b3b71531f9c2ed6f4a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a2fa0615260e49e5e9f934d4f8e0f6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a2229312107571592c6",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "491fc4e21c9ff810891e",
+    "url": "/static/css/main.4e35b41e.chunk.css"
+  },
+  {
+    "revision": "2a2229312107571592c6",
+    "url": "/static/js/2.00d8a6de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00d8a6de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "491fc4e21c9ff810891e",
+    "url": "/static/js/main.556b7412.chunk.js"
+  },
+  {
+    "revision": "b83cbc8f1b0516bb5a45",
+    "url": "/static/js/runtime-main.39d042f1.js"
+  }
+]);
